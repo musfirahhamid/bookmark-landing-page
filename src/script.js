f858397dcb7986398d052d76368b43875b9d42ef
Musfirah Hamid <29musfirah@gmail.com>
2022-05-18 23:53:34 +0100

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,35 @@
+const buttons = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+
+buttons.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
+
+//tabs
+$('.tab-link').click( function() {
+	
+	var tabID = $(this).attr('data-tab');
+	
+	$(this).addClass('active').siblings().removeClass('active');
+	
+	$('#tab-'+tabID).addClass('active').siblings().removeClass('active');
+});
+
+//email validation
+const email = document.querySelector('input[name=email]');
+const button = document.querySelector('#btn');
+const text =  document.querySelector('#message');
+
+const validateEmail= (email) => {
+    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+}
+
+button.addEventListener('click',()=>{
+    if(validateEmail(email.value)){
+      text.innerText="We will Respond you soon";
+    }else{
+      text.innerText="Whoops, Make sure it's an email";
+    }
+})
